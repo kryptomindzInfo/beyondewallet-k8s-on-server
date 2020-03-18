bb97f3e5893260145bd039cd1b37df2d86e0760e
Zainab M <hlf-dev-zm>
2020-03-18 12:08:33 +0000

    updated pids fetching

--- kubernetes/port-forward.sh
+++ kubernetes/port-forward.sh
@@ -1,26 +1,38 @@
+ps -auxf | grep 'kubectl'
+echo "********************ingress-nginx*************************"
 if ! curl --output - http://localhost/ --header 'HOST: 91d90ac373dc.sn.mynetname.net'
  then
-     if [ ! -z $_pid1 ]; then   kill $_pid1; fi
+
+     _pid1=`/bin/ps -fu $USER| grep "svc/ingress-nginx" | grep -v "grep" | awk '{print $2}'`
+     echo $_pid1
+     if [ ! -z $_pid1 ]; then kill $_pid1; fi
      /usr/local/bin/kubectl port-forward svc/ingress-nginx -n ingress-nginx 80:80 --address='0.0.0.0' --request-timeout='0' &
-     _pid1=$!
-       	
 fi
-if ! curl -I --output - http://localhost:3000/
+echo "********************grafana-nodeport*************************"
+if ! curl --output - http://localhost:3000/login
  then
+     _pid2=`/bin/ps -fu $USER| grep "svc/grafana-nodeport" | grep -v "grep" | awk '{print $2}'`
+     echo $_pid2
      if [ ! -z $_pid2 ]; then kill $_pid2; fi
      /usr/local/bin/kubectl port-forward svc/grafana-nodeport 3000:3000 --address='0.0.0.0' --request-timeout='0' &
      _pid2=$!
 
 fi
-if ! curl -I --output - http://localhost:9090/
+echo "********************prometheus-nodeport*************************"
+if ! curl --output - http://localhost:9090/
  then
+     _pid3=`/bin/ps -fu $USER| grep "svc/prometheus-nodeport" | grep -v "grep" | awk '{print $2}'`
+     echo $_pid3
      if [ ! -z $_pid3 ]; then kill $_pid3; fi
      /usr/local/bin/kubectl port-forward svc/prometheus-nodeport 9090:9090 --address='0.0.0.0' --request-timeout='0' &
      _pid3=$!
 
 fi
-if ! curl -I --output - http://localhost:8080/
+echo "********************explorer-nodeport*************************"
+if ! curl --output - http://localhost:8080/
  then
+     _pid4=`/bin/ps -fu $USER| grep "svc/explorer-nodeport" | grep -v "grep" | awk '{print $2}'`
+     echo $_pid4
      if [ ! -z $_pid4 ]; then kill $_pid4; fi
      /usr/local/bin/kubectl port-forward svc/explorer-nodeport 8080:8080 --address='0.0.0.0' --request-timeout='0' &
      _pid4=$!
