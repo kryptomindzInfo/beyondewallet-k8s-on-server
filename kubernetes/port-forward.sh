139c0e34e8cc2771455d50763f74127d448c02fc
hlf-dev-zm <bzainab61@gmail.com>
2020-03-20 04:30:12 +0000

    port forward curl command update

--- kubernetes/port-forward.sh
+++ kubernetes/port-forward.sh
@@ -1,6 +1,8 @@
+echo $(date)
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
 ps -auxf | grep 'kubectl'
 echo "********************ingress-nginx*************************"
-if ! curl --output - http://localhost/ --header 'HOST: 91d90ac373dc.sn.mynetname.net'
+if ! curl -s -o /dev/null -I -L -w "\n%{http_code}\n" --output - http://localhost/ --header 'HOST: 91d90ac373dc.sn.mynetname.net'
  then
 
      _pid1=`/bin/ps -fu $USER| grep "svc/ingress-nginx" | grep -v "grep" | awk '{print $2}'`
@@ -9,7 +11,7 @@ if ! curl --output - http://localhost/ --header 'HOST: 91d90ac373dc.sn.mynetname
      /usr/local/bin/kubectl port-forward svc/ingress-nginx -n ingress-nginx 80:80 --address='0.0.0.0' --request-timeout='0' &
 fi
 echo "********************grafana-nodeport*************************"
-if ! curl --output - http://localhost:3000/login
+if ! curl -s -o /dev/null -I -L -w "\n%{http_code}\n" --output - http://localhost:3000/login
  then
      _pid2=`/bin/ps -fu $USER| grep "svc/grafana-nodeport" | grep -v "grep" | awk '{print $2}'`
      echo $_pid2
@@ -19,7 +21,7 @@ if ! curl --output - http://localhost:3000/login
 
 fi
 echo "********************prometheus-nodeport*************************"
-if ! curl --output - http://localhost:9090/
+if ! curl -s -o /dev/null -L -w "\n%{http_code}\n" --output - http://localhost:9090/graph
  then
      _pid3=`/bin/ps -fu $USER| grep "svc/prometheus-nodeport" | grep -v "grep" | awk '{print $2}'`
      echo $_pid3
@@ -29,7 +31,7 @@ if ! curl --output - http://localhost:9090/
 
 fi
 echo "********************explorer-nodeport*************************"
-if ! curl --output - http://localhost:8080/
+if ! curl -s -o /dev/null -I -L -w "\n%{http_code}\n" --output - http://localhost:8080/
  then
      _pid4=`/bin/ps -fu $USER| grep "svc/explorer-nodeport" | grep -v "grep" | awk '{print $2}'`
      echo $_pid4
@@ -38,3 +40,6 @@ if ! curl --output - http://localhost:8080/
      _pid4=$!
 
 fi
+echo ""
+echo "----------------------------------------------------------------------------------------------------------------------------------------------"
+echo ""
