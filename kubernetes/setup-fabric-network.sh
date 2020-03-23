72a2ce1461cf1d4f6b6cb5a91fb97b555b14b205
hlf-dev-zm <bzainab61@gmail.com>
2020-03-23 10:45:27 +0000

    absolute path for config

--- kubernetes/setup-fabric-network.sh
+++ kubernetes/setup-fabric-network.sh
@@ -5,11 +5,11 @@
 while [[ $(kubectl get pods siliconvalley-peer1-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 
 kubectl exec -it siliconvalley-peer1-0 -c siliconvalley-peer1 -- /bin/bash -c ' \
-peer channel create -c $CC_CHANNEL_ID -f ../config/${CC_CHANNEL_ID}.tx --outputBlock ../config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS --tls --cafile $ORDERER_CA && \
-peer channel join   -b ../config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS  && \
+peer channel create -c $CC_CHANNEL_ID -f /var/hyperledger/config/${CC_CHANNEL_ID}.tx --outputBlock /var/hyperledger/config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS --tls --cafile $ORDERER_CA && \
+peer channel join   -b /var/hyperledger/config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS  && \
 echo "List of all channels on peer"  && \
 peer channel list  && \
-peer channel update -f ../config/siliconvalley-peer-update.tx -c $CC_CHANNEL_ID -o $ORDERER_ADDRESS  --tls --cafile $ORDERER_CA && \
+peer channel update -f /var/hyperledger/config/siliconvalley-peer-update.tx -c $CC_CHANNEL_ID -o $ORDERER_ADDRESS  --tls --cafile $ORDERER_CA && \
 peer chaincode install  -n $CC_NAME -p $CC_PATH -v $CC_VERSION && \
 peer chaincode list --installed -C $CC_CHANNEL_ID && \
 peer chaincode instantiate -o siliconvalley-orderer-clusterip:30750 -C $CC_CHANNEL_ID -n $CC_NAME  -v $CC_VERSION -c "{\"Args\":[]}" --tls --cafile $ORDERER_CA'
@@ -18,8 +18,8 @@ peer chaincode instantiate -o siliconvalley-orderer-clusterip:30750 -C $CC_CHANN
 ############### peer2 ####################
 while [[ $(kubectl get pods siliconvalley-peer2-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 kubectl exec -it siliconvalley-peer2-0 -c siliconvalley-peer2 -- /bin/bash -c '
-peer channel fetch 0 ../config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS -c $CC_CHANNEL_ID  --tls --cafile $ORDERER_CA && \
-peer channel join -b ../config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS && \
+peer channel fetch 0 /var/hyperledger/config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS -c $CC_CHANNEL_ID  --tls --cafile $ORDERER_CA && \
+peer channel join -b /var/hyperledger/config/${CC_CHANNEL_ID}.block -o $ORDERER_ADDRESS && \
 echo "List of all channels on peer" && \
 peer channel list && \
 peer chaincode install -n $CC_NAME -p $CC_PATH -v $CC_VERSION && \
