639882614c737880b96d231e5eccf7e4f7856d80
zainabmurtaza <bzainab61@gmail.com>
2020-07-14 18:02:26 +0530

    directory added as input

--- kubernetes/restart-app.sh
+++ kubernetes/restart-app.sh
@@ -1,11 +1,16 @@
-./teardown-app.sh
-kubectl apply -f ./minikube/k8s-storage-class.yaml
-kubectl apply -f ./minikube/k8s-beyondewallet-app-fabric.yaml
-kubectl apply -f ./minikube/k8s-beyondewallet-app-middleware.yaml
-kubectl apply -f ./minikube/k8s-beyondewallet-app-frontend.yaml
+DIR=$1
 
-kubectl apply -f ./minikube/k8s-nginx-ingress.yaml
-kubectl apply -f ./minikube/k8s-http-load-balancer.yaml
+./teardown-app.sh
+
+# kubectl apply -f ./$DIR/k8s-storage-class.yaml
+kubectl apply -f ./$DIR/k8s-beyondewallet-app-fabric.yaml
+kubectl apply -f ./$DIR/k8s-beyondewallet-app-middleware.yaml
+kubectl apply -f ./$DIR/k8s-beyondewallet-mongodb.yaml
+kubectl apply -f ./$DIR/k8s-beyondewallet-app-frontend.yaml
+kubectl apply -f ./$DIR/k8s-beyondewallet-app-cust-frontend.yaml
+
+# kubectl apply -f ./$DIR/k8s-nginx-ingress.yaml
+# kubectl apply -f ./$DIR/k8s-http-load-balancer.yaml
 
 # sudo -s
 # while true; do kubectl port-forward svc/ingress-nginx -n ingress-nginx 80:80 --address='0.0.0.0'; done &
