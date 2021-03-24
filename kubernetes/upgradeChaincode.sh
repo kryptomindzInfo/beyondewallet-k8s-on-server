60a4d756ebfcc4bff1b499a4ebaf5890ea8858eb
hlf-dev-zm <bzainab61@gmail.com>
2021-03-24 20:05:55 +0530

    Added new queue and also added a script to upgrade chaincpde

--- kubernetes/upgradeChaincode.sh
+++ kubernetes/upgradeChaincode.sh
@@ -7,11 +7,11 @@ kubectl cp ../fabric-network/ewallet-cc/ewallet.go siliconvalley-peer2-0:/opt/go
 
 ############# upgrade in peer1 ####################
 kubectl exec -it siliconvalley-peer1-0 -c siliconvalley-peer1 -- /bin/bash -li -c ' \
-peer chaincode install -n $CC_NAME -p $CC_PATH -v "$1" && \
+peer chaincode install -n $CC_NAME -p $CC_PATH -v $1 && \
 peer chaincode list --installed -C $CC_CHANNEL_ID && \
-peer chaincode instantiate -o siliconvalley-orderer-clusterip:30750 -C $CC_CHANNEL_ID -n $CC_NAME  -v "$1" -c "{\"Args\":[]}" --tls --cafile $ORDERER_CA' _ $VER
+peer chaincode upgrade -o siliconvalley-orderer-clusterip:30750 -C $CC_CHANNEL_ID -n $CC_NAME  -v $1 -c "{\"Args\":[]}" --tls --cafile $ORDERER_CA' _ $VER
 
 ############### upgrade in peer2 ####################
 kubectl exec -it siliconvalley-peer2-0 -c siliconvalley-peer2 -- /bin/bash -li -c '
-peer chaincode install -n $CC_NAME -p $CC_PATH -v "$1" && \
+peer chaincode install -n $CC_NAME -p $CC_PATH -v $1 && \
 peer chaincode list --installed -C $CC_CHANNEL_ID' _ $VER
